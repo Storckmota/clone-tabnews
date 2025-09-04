88662fedea832784455e0648397f09fb18a6fea3
Julio Mota <87203157+Storckmota@users.noreply.github.com>
2025-09-04 20:59:36 +0000

    adicionando .gitignore

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+function Home(){
+
+return <h1>"Os portões do inferno estão abertos noite e dia; Suave a descida, e fácil é o caminho: Mas retornar e ver os céus alegres, Nisto reside a tarefa e o poderoso trabalho." -Virgílio</h1>;
+
+};
+
+
+export default Home; 
