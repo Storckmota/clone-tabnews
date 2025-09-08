cc5025043da603f4ce4105743ad6434d01efd6fa
Julio Mota <87203157+Storckmota@users.noreply.github.com>
2025-09-08 17:32:26 +0000

    adiciona figurinha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-return <h1>"Os portões do inferno estão abertos noite e dia; Suave a descida, e fácil é o caminho: Mas retornar e ver os céus alegres, Nisto reside a tarefa e o poderoso trabalho." -Virgílio</h1>;
+return <h1>"Os portões do inferno estão abertos noite e dia; Suave a descida, e fácil é o caminho: Mas retornar e ver os céus alegres, Nisto reside a tarefa e o poderoso trabalho. ⛩ " -Virgílio</h1>;
 
 };
 
