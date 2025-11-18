fba4b158109cffdc89ddd1959cff06c9e59c53c2
Julio Mota <87203157+Storckmota@users.noreply.github.com>
2025-11-18 19:29:51 +0000

    add database.js and .env files

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,11 @@
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result);
+  response
+    .status(200)
+    .json({ chave: "alunos do curso.dev são pessoas acima da media" });
+}
+
+export default status;
